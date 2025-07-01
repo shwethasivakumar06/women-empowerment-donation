09ff84da65f13ab70f9c4246e29611333ca47869
unknown <shwethasivakumar9@gmail.com>
2025-07-01 20:20:00 +0530

    Describe your changes, e.g. Hide navbar on signup/login, fix routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Programs from "./pages/Programs";
 import Donate from "./pages/Donate";
